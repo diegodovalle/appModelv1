d7705305321fe60922cf11d062b311789352004e
diegodovalle <60814762+diegodovalle@users.noreply.github.com>
2023-03-22 17:16:24 -0300

    5 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { SocialSharing } from '@ionic-native/social-sharing/ngx';
+import { File } from '@ionic-native/file/ngx';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+    StatusBar,
+    SplashScreen,
+    SocialSharing,
+    File,
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { SocialSharing } from '@ionic-native/social-sharing/ngx';
+import { File } from '@ionic-native/file/ngx';
 
 @Component({
   selector: 'app-tab1',
@@ -6,5 +8,29 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss'],
 })
 export class Tab1Page {
-  constructor() {}
+  constructor(private socialSharing: SocialSharing, private file: File) {}
+
+  compartilharTexto() {
+    this.socialSharing.share('Compartilhando texto');
+  }
+
+  compartilharLink() {
+    this.socialSharing.share('', '', '', 'https://www.abrconsultoria.com.br');
+  }
+
+  async compartilharImagem() {
+    let lisaImg = await this.resolveLocalFile();
+    this.socialSharing
+      .share('', '', lisaImg.nativeURL)
+      .then((s) => {
+        this.file.removeFile(this.file.cacheDirectory, lisaImg.name);
+      })
+      .catch((e) => {
+        alert('Erro: ' + e);
+      });
+  }
+
+  async resolveLocalFile() {
+    return this.file.copyFile(`${this.file.applicationDirectory}www/assets/`);
+  }
 }
