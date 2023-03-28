db6287c5b7d4dad338c5a840e2bdb1c2b4fe82ab
diegodovalle <60814762+diegodovalle@users.noreply.github.com>
2023-03-28 13:50:16 -0300

    5 files modified

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { Share } from '@capacitor/share';
 
 @Component({
   selector: 'app-tab1',
@@ -7,16 +6,5 @@ import { Share } from '@capacitor/share';
   styleUrls: ['tab1.page.scss'],
 })
 export class Tab1Page {
-  result: string;
-
   constructor() {}
-
-  async share() {
-    await Share.share({
-      title: 'See cool stuff',
-      text: 'Really awesome thing you need to see right meow',
-      url: 'http://ionicframework.com/',
-      dialogTitle: 'Share with buddies',
-    });
-  }
 }
